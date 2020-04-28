2ffea2143a057110dff6074de0d6c974f15b18e8
hendraptr <hendraptr@yahoo.com>
2020-04-28 22:29:09 +0800

    Clean up Session.java

--- src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -7,25 +7,25 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "sessions")
-@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Session {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long session_id;
-
     private String session_name;
     private String session_description;
     private Integer session_length;
 
     @ManyToMany
     @JoinTable(
-            name="session_speakers",
-            joinColumns = @JoinColumn(name="session_id"),
-            inverseJoinColumns = @JoinColumn(name="speaker_id"))
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id"))
     private List<Speaker> speakers;
 
-    public Session(){
+    public Session() {
     }
+
 
     public List<Speaker> getSpeakers() {
         return speakers;
